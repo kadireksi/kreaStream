5ef746036ad5164c48e07832f82e598f7714e4c5
Kadir Ekşi <eksi.kadir@gmail.com>
2025-09-19 20:18:02 +0300

    Update CanliDizi.kt

--- CanliDizi/src/main/kotlin/com/kreastream/CanliDizi.kt
+++ CanliDizi/src/main/kotlin/com/kreastream/CanliDizi.kt
@@ -23,7 +23,7 @@ class CanliDizi : MainAPI() {
         val homeSections = mutableListOf<HomePageList>()
 
         // Popular Series section
-        val popularSeries = document.select("div.diziler div.list-series").mapNotNull { element ->
+        val popularSeries = document.select("div.list-series").mapNotNull { element ->
             parseSeriesItem(element)
         }.takeIf { it.isNotEmpty() }?.let {
             HomePageList("Popüler Diziler", it)
