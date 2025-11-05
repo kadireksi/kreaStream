87905196ad17223e557e65af09e8b6b536f75521
Kadir Ekşi <eksi.kadir@gmail.com>
2025-11-05 21:45:57 +0300

    Update TrMain.kt

--- TR/src/main/kotlin/com/kreastream/TrMain.kt
+++ TR/src/main/kotlin/com/kreastream/TrMain.kt
@@ -2,6 +2,8 @@ package com.kreastream
 
 import com.lagradost.cloudstream3.Plugin
 import com.lagradost.cloudstream3.main
+import com.kreastream.modules.Trt1Parser
+import com.kreastream.modules.TrtLive
 
 class TrMain : Plugin() {
     override fun load() {
