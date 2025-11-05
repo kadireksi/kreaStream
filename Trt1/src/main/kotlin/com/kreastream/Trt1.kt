1d9c79d18a7e5293e50c87f267c25d4710ebfb8b
Kadir Ekşi <eksi.kadir@gmail.com>
2025-11-05 13:51:06 +0300

    Update Trt1.kt

--- Trt1/src/main/kotlin/com/kreastream/Trt1.kt
+++ Trt1/src/main/kotlin/com/kreastream/Trt1.kt
@@ -1,4 +1,4 @@
-package com.lagradost.cloudstream3.trt1
+package com.kreastream
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
@@ -6,9 +6,10 @@ import com.lagradost.cloudstream3.utils.*
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
-class TRT1Provider : MainAPI() {
+class Trt1 : MainAPI() {
     override var mainUrl = "https://www.trt1.com.tr"
     override var name = "TRT1"
+    override var language = "tr"
     override val hasMainPage = true
     override val supportedTypes = setOf(TvType.TvSeries)
 
