cddcbfd5d630d6901eac58e7a7aa801695ccbdb9
Kadir Ekşi <eksi.kadir@gmail.com>
2025-11-06 00:37:50 +0300

    Update TrPlugin.kt

--- TR/src/main/kotlin/com/kreastream/TrPlugin.kt
+++ TR/src/main/kotlin/com/kreastream/TrPlugin.kt
@@ -3,7 +3,7 @@ package com.kreastream
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.lagradost.cloudstream3.plugins.registerMainAPI
+
 
 @CloudstreamPlugin
 class TrPlugin : Plugin() {
