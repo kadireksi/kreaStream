74849fcd3bf01c74f5e3fc955920a9a1a572476d
Kadir Ekşi <eksi.kadir@gmail.com>
2025-11-06 13:36:14 +0300

    Add files via upload

--- TR/src/main/kotlin/com/kreastream/TrPlugin.kt
+++ TR/src/main/kotlin/com/kreastream/TrPlugin.kt
@@ -3,12 +3,11 @@ package com.kreastream
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.lagradost.cloudstream3.plugins.registerMainAPI
 
 @CloudstreamPlugin
 class TrPlugin : Plugin() {
     override fun load(context: Context) {
-        // Register a single unified main API
-       this.registerMainAPI(TrMain())
+        // Register your unified API manually
+        addMainAPI(TrMain())
     }
 }
