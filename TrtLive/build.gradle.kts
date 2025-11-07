18bae92ec370e0552a5057ddb63f8cf873abe5b5
Kadir Ekşi <eksi.kadir@gmail.com>
2025-11-08 02:18:05 +0300

    Update build.gradle.kts

--- TrtLive/build.gradle.kts
+++ TrtLive/build.gradle.kts
@@ -1,10 +1,9 @@
 version = 1
 
 cloudstream {
-    author = "kreastream"
+    authors = "kreastream"
     language = "tr"
     description = "TRT Canlı yayını ve TRT dizileri"
     status = 1
-    tvTypes = setOf(TvType.TvSeries, TvType.Live)
     iconUrl = "https://raw.githubusercontent.com/kadireksi/kreaStream/refs/heads/logos/TV/trt.png"  
 }
