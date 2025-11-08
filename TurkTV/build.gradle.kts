40a1ff2bef1fa21db1250244222c766408a3c5c1
Kadir Ekşi <eksi.kadir@gmail.com>
2025-11-08 21:07:09 +0300

    Add files via upload

--- TurkTV/build.gradle.kts
+++ TurkTV/build.gradle.kts
@@ -4,6 +4,5 @@ cloudstream {
     language = "tr"
     description = "Türk TV Dizi ve Canlı Yayınları"
     status = 1
-    tvTypes = setOf(TvType.TvSeries, TvType.Live)
     iconUrl = "https://raw.githubusercontent.com/kadireksi/kreaStream/refs/heads/logos/tr_flag.png"  
 }
