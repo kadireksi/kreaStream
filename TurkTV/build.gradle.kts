bffb46320a3f18cf59440007e12bcc195eb31888
Kadir Ekşi <eksi.kadir@gmail.com>
2025-11-08 21:38:55 +0300

    Delete TurkTV directory

--- TurkTV/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-version = 1
-
-cloudstream {
-    language = "tr"
-    description = "Türk TV Dizi ve Canlı Yayınları"
-    status = 1
-    iconUrl = "https://raw.githubusercontent.com/kadireksi/kreaStream/refs/heads/logos/tr_flag.png"  
-}
