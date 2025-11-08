2baf39a6b82ce9317cc797f3e3b1b90900b38deb
Kadir Ekşi <eksi.kadir@gmail.com>
2025-11-08 21:38:48 +0300

    Delete TrtLive directory

--- TrtLive/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-version = 1
-
-cloudstream {
-    language = "tr"
-    description = "TRT Canlı yayını ve TRT dizileri"
-    status = 1
-    iconUrl = "https://raw.githubusercontent.com/kadireksi/kreaStream/refs/heads/logos/TV/trt.png"  
-}
