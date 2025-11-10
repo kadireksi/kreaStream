91749af51de5fe391b71ec4e998f66d2975ef836
Kadir Ekşi <eksi.kadir@gmail.com>
2025-11-10 21:13:48 +0300

    Add files via upload

--- Trt/build.gradle.kts
+++ Trt/build.gradle.kts
@@ -1,5 +1,11 @@
 version = 1
 
 cloudstream {
-    status = 1
+    authors     = listOf("kreastream")
+    language    = "tr"
+    description = "TRT Canlı Yayın ve Diziler"
+    status  = 1
+    tvTypes = listOf("TvSeries", "Live")
+    iconUrl = "https://trt.net.tr/favicon.ico"
 }
+
