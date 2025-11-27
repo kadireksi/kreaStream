4d9cfffc27e857f5e7ba493b23c8d5e0a9305ea8
Kadir Ekşi <eksi.kadir@gmail.com>
2025-11-27 20:29:40 +0300

    Update build.gradle.kts

--- HDFilmCehennemi/build.gradle.kts
+++ HDFilmCehennemi/build.gradle.kts
@@ -1,18 +1,10 @@
-version = 26
+version = 2
 
 cloudstream {
-    authors     = listOf("hexated", "keyiflerolsun", "kraptor123")
+    authors     = listOf("kreastream")
     language    = "tr"
     description = "Türkiye'nin en hızlı hd film izleme sitesi"
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-    **/
-    status  = 1 // will be 3 if unspecified
+    status  = 1
     tvTypes = listOf("Movie", "TvSeries")
     iconUrl = "https://www.google.com/s2/favicons?domain=hdfilmcehennemi.com&sz=%size%"
 }
