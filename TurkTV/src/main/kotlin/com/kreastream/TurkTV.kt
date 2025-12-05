e1edd7ad6484427de74af306cd9956a8f2bb9050
Kadir Ekşi <eksi.kadir@gmail.com>
2025-12-06 02:27:50 +0300

    Update TurkTV.kt

--- TurkTV/src/main/kotlin/com/kreastream/TurkTV.kt
+++ TurkTV/src/main/kotlin/com/kreastream/TurkTV.kt
@@ -290,8 +290,7 @@ override suspend fun loadLinks(
             .attr("src")
             .ifBlank { doc.select("video source[src$='.mp4']").attr("src") }
             .ifBlank { Regex("""https?://[^\s"']+\.(?:m3u8|mp4)""").find(doc.html())?.value }
-            .ifBlank { doc.select("iframe[src*='.m3u8'], iframe[src*='.mp4']").firstOrNull()?.attr("src") ?: ""}
-
+            .ifBlank { val iframe = doc.select("iframe[src*='.m3u8'], iframe[src*='.mp4']").firstOrNull()iframe?.attr("src") ?: ""}
         if (!streamUrl.isNullOrBlank()) {
             val finalUrl = full(cfg.baseUrl, streamUrl) ?: streamUrl
             
