3d77c894afeddd4440bab2cefe7e32faabe82cf8
Kadir Ekşi <eksi.kadir@gmail.com>
2025-12-06 10:22:22 +0300

    Update TurkTV.kt

--- TurkTV/src/main/kotlin/com/kreastream/TurkTV.kt
+++ TurkTV/src/main/kotlin/com/kreastream/TurkTV.kt
@@ -290,7 +290,7 @@ class TurkTV : MainAPI() {
                 .attr("src")
                 .ifBlank { doc.select("video source[src$='.mp4']").attr("src") }
                 .ifBlank { Regex("""https?://[^\s"']+\.(?:m3u8|mp4)""").find(doc.html())?.value }
-                .ifBlank { doc.select("iframe[src*='.m3u8'], iframe[src*='.mp4']").attr("src") }
+                .ifBlank { doc.select("iframe[src*='.m3u8'], iframe[src*='.mp4']").attr("src")? }
 
             if (!streamUrl.isNullOrBlank()) {
                 val finalUrl = full(cfg.baseUrl, streamUrl) ?: streamUrl
