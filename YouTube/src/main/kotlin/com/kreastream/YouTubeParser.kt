9c562df37be619137cc796e4185fd20527c95966
Kadir Ekşi <eksi.kadir@gmail.com>
2025-12-17 22:10:41 +0300

    Update YouTubeParser.kt

--- YouTube/src/main/kotlin/com/kreastream/YouTubeParser.kt
+++ YouTube/src/main/kotlin/com/kreastream/YouTubeParser.kt
@@ -3,58 +3,45 @@ package com.kreastream
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import org.schabi.newpipe.extractor.ServiceList
+import org.schabi.newpipe.extractor.channel.ChannelInfo
+import org.schabi.newpipe.extractor.playlist.PlaylistInfo
 import org.schabi.newpipe.extractor.stream.StreamInfo
 import org.schabi.newpipe.extractor.stream.StreamInfoItem
-import org.schabi.newpipe.extractor.channel.ChannelInfo
-import org.schabi.newpipe.extractor.channel.ChannelInfoItem
-import org.schabi.newpipe.extractor.playlist.PlaylistInfo
-import org.schabi.newpipe.extractor.playlist.PlaylistInfoItem
 import org.schabi.newpipe.extractor.search.SearchInfo
-import org.schabi.newpipe.extractor.search.SearchInfoItem
-import org.schabi.newpipe.extractor.search.filter.SearchFilters
-import org.schabi.newpipe.extractor.search.filter.SearchFilters.FilterType
-import org.schabi.newpipe.extractor.linkhandler.LinkHandlerFactory
 
-class YouTubeParser(private val apiName: String) {
+class YouTubeParser(
+    private val api: MainAPI,
+    private val apiName: String
+) {
 
     init {
         ServiceList.YouTube.init(NewPipeDownloader.getInstance())
     }
 
-    /* ========================= SEARCH ========================= */
+    /* ================= SEARCH ================= */
 
     fun search(query: String, contentFilter: String): List<SearchResponse> {
-        val filters = SearchFilters.Builder().apply {
-            when (contentFilter) {
-                "channels" -> setType(FilterType.CHANNEL)
-                "playlists" -> setType(FilterType.PLAYLIST)
-                else -> setType(FilterType.VIDEO)
-            }
-        }.build()
+        val searchInfo = SearchInfo.getInfo(ServiceList.YouTube, query)
 
-        val searchInfo = SearchInfo.getInfo(
-            ServiceList.YouTube,
-            query,
-            filters,
-            null
-        )
-
-        return searchInfo.items.mapNotNull { item ->
-            when (item) {
-                is StreamInfoItem -> videoToSearchResponse(item)
-                is ChannelInfoItem -> channelToSearchResponse(item)
-                is PlaylistInfoItem -> playlistToSearchResponse(item)
-                else -> null
+        return searchInfo.relatedItems
+            .filterIsInstance<StreamInfoItem>()
+            .map {
+                api.newMovieSearchResponse(
+                    name = it.name ?: "YouTube",
+                    url = it.url,
+                    type = TvType.Others
+                ) {
+                    posterUrl = it.thumbnails.lastOrNull()?.url
+                }
             }
-        }
     }
 
-    /* ========================= LOAD ========================= */
+    /* ================= LOAD ================= */
 
     suspend fun videoToLoadResponse(url: String): LoadResponse {
         val info = StreamInfo.getInfo(ServiceList.YouTube, url)
 
-        return newMovieLoadResponse(
+        return api.newMovieLoadResponse(
             name = info.name ?: "YouTube Video",
             url = url,
             type = TvType.Others,
@@ -72,13 +59,13 @@ class YouTubeParser(private val apiName: String) {
         val episodes = info.relatedItems
             .filterIsInstance<StreamInfoItem>()
             .map {
-                newEpisode(it.url) {
+                api.newEpisode(it.url) {
                     name = it.name
                     posterUrl = it.thumbnails.lastOrNull()?.url
                 }
             }
 
-        return newTvSeriesLoadResponse(
+        return api.newTvSeriesLoadResponse(
             name = info.name ?: "YouTube Channel",
             url = url,
             type = TvType.Others,
@@ -95,13 +82,13 @@ class YouTubeParser(private val apiName: String) {
         val episodes = info.relatedItems
             .filterIsInstance<StreamInfoItem>()
             .map {
-                newEpisode(it.url) {
+                api.newEpisode(it.url) {
                     name = it.name
                     posterUrl = it.thumbnails.lastOrNull()?.url
                 }
             }
 
-        return newTvSeriesLoadResponse(
+        return api.newTvSeriesLoadResponse(
             name = info.name ?: "YouTube Playlist",
             url = url,
             type = TvType.Others,
@@ -112,32 +99,26 @@ class YouTubeParser(private val apiName: String) {
         }
     }
 
-    /* ========================= SEARCH HELPERS ========================= */
+    /* ================= MAIN PAGE ================= */
 
-    private fun videoToSearchResponse(item: StreamInfoItem): SearchResponse =
-        newMovieSearchResponse(
-            name = item.name ?: "YouTube Video",
-            url = item.url,
-            type = TvType.Others
-        ) {
-            posterUrl = item.thumbnails.lastOrNull()?.url
-        }
+    fun getTrendingVideoUrls(page: Int): HomePageList {
+        val search = SearchInfo.getInfo(ServiceList.YouTube, "trending")
 
-    private fun channelToSearchResponse(item: ChannelInfoItem): SearchResponse =
-        newTvSeriesSearchResponse(
-            name = item.name ?: "YouTube Channel",
-            url = item.url,
-            type = TvType.Others
-        ) {
-            posterUrl = item.thumbnails.lastOrNull()?.url
-        }
+        val items = search.relatedItems
+            .filterIsInstance<StreamInfoItem>()
+            .map {
+                api.newMovieSearchResponse(
+                    name = it.name ?: "YouTube",
+                    url = it.url,
+                    type = TvType.Others
+                ) {
+                    posterUrl = it.thumbnails.lastOrNull()?.url
+                }
+            }
 
-    private fun playlistToSearchResponse(item: PlaylistInfoItem): SearchResponse =
-        newTvSeriesSearchResponse(
-            name = item.name ?: "YouTube Playlist",
-            url = item.url,
-            type = TvType.Others
-        ) {
-            posterUrl = item.thumbnails.lastOrNull()?.url
-        }
+        return HomePageList(
+            listOf(HomePageList.HomePage("Trending", items)),
+            hasNext = false
+        )
+    }
 }
