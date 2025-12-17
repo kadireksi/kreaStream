512ce81d9bfec7292af0bd004815fb321f41be85
Kadir Ekşi <eksi.kadir@gmail.com>
2025-12-17 22:27:03 +0300

    Update YouTubeParser.kt

--- YouTube/src/main/kotlin/com/kreastream/YouTubeParser.kt
+++ YouTube/src/main/kotlin/com/kreastream/YouTubeParser.kt
@@ -5,29 +5,68 @@ import com.lagradost.cloudstream3.utils.*
 import org.schabi.newpipe.extractor.ServiceList
 import org.schabi.newpipe.extractor.channel.ChannelInfo
 import org.schabi.newpipe.extractor.playlist.PlaylistInfo
+import org.schabi.newpipe.extractor.search.SearchInfo
 import org.schabi.newpipe.extractor.stream.StreamInfo
 import org.schabi.newpipe.extractor.stream.StreamInfoItem
-import org.schabi.newpipe.extractor.search.SearchInfo
 
 class YouTubeParser(
     private val api: MainAPI,
     private val apiName: String
 ) {
 
-    init {
-        ServiceList.YouTube.init(NewPipeDownloader.getInstance())
-    }
-
     /* ================= SEARCH ================= */
 
     fun search(query: String, contentFilter: String): List<SearchResponse> {
-        val searchInfo = SearchInfo.getInfo(ServiceList.YouTube, query)
+        return when (contentFilter) {
+            "channels" -> channelToSearchResponseList(query)
+            "playlists" -> playlistToSearchResponseList(query)
+            else -> videoSearch(query)
+        }
+    }
 
-        return searchInfo.relatedItems
+    private fun videoSearch(query: String): List<SearchResponse> {
+        val qh = ServiceList.YouTube.searchQHFactory.fromQuery(query)
+        val info = SearchInfo.getInfo(ServiceList.YouTube, qh)
+
+        return info.items
             .filterIsInstance<StreamInfoItem>()
             .map {
                 api.newMovieSearchResponse(
                     name = it.name ?: "YouTube",
+                    url = it.url,
+                    type = TvType.Others
+                ) {
+                    posterUrl = it.thumbnails.lastOrNull()?.url
+                }
+            }
+    }
+
+    fun channelToSearchResponseList(query: String): List<SearchResponse> {
+        val qh = ServiceList.YouTube.searchQHFactory.fromQuery(query)
+        val info = SearchInfo.getInfo(ServiceList.YouTube, qh)
+
+        return info.items
+            .filterIsInstance<org.schabi.newpipe.extractor.channel.ChannelInfoItem>()
+            .map {
+                api.newTvSeriesSearchResponse(
+                    name = it.name ?: "YouTube Channel",
+                    url = it.url,
+                    type = TvType.Others
+                ) {
+                    posterUrl = it.thumbnails.lastOrNull()?.url
+                }
+            }
+    }
+
+    fun playlistToSearchResponseList(query: String): List<SearchResponse> {
+        val qh = ServiceList.YouTube.searchQHFactory.fromQuery(query)
+        val info = SearchInfo.getInfo(ServiceList.YouTube, qh)
+
+        return info.items
+            .filterIsInstance<org.schabi.newpipe.extractor.playlist.PlaylistInfoItem>()
+            .map {
+                api.newTvSeriesSearchResponse(
+                    name = it.name ?: "YouTube Playlist",
                     url = it.url,
                     type = TvType.Others
                 ) {
@@ -56,7 +95,7 @@ class YouTubeParser(
     suspend fun channelToLoadResponse(url: String): LoadResponse {
         val info = ChannelInfo.getInfo(ServiceList.YouTube, url)
 
-        val episodes = info.relatedItems
+        val episodes = info.items
             .filterIsInstance<StreamInfoItem>()
             .map {
                 api.newEpisode(it.url) {
@@ -79,7 +118,7 @@ class YouTubeParser(
     suspend fun playlistToLoadResponse(url: String): LoadResponse {
         val info = PlaylistInfo.getInfo(ServiceList.YouTube, url)
 
-        val episodes = info.relatedItems
+        val episodes = info.items
             .filterIsInstance<StreamInfoItem>()
             .map {
                 api.newEpisode(it.url) {
@@ -102,9 +141,10 @@ class YouTubeParser(
     /* ================= MAIN PAGE ================= */
 
     fun getTrendingVideoUrls(page: Int): HomePageList {
-        val search = SearchInfo.getInfo(ServiceList.YouTube, "trending")
+        val qh = ServiceList.YouTube.searchQHFactory.fromQuery("trending")
+        val info = SearchInfo.getInfo(ServiceList.YouTube, qh)
 
-        val items = search.relatedItems
+        val items = info.items
             .filterIsInstance<StreamInfoItem>()
             .map {
                 api.newMovieSearchResponse(
@@ -117,8 +157,7 @@ class YouTubeParser(
             }
 
         return HomePageList(
-            listOf(HomePageList.HomePage("Trending", items)),
-            hasNext = false
+            listOf(HomePageList.HomePage("Trending", items))
         )
     }
 }
