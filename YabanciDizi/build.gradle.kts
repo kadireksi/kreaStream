7aa95c32e8e7840b6571da9a6578b6c7999892b6
Kadir Ekşi <eksi.kadir@gmail.com>
2025-12-20 01:18:42 +0300

    Add files via upload

---
+++ YabanciDizi/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 251220
+
+cloudstream {
+    authors     = listOf("kreastream")
+    language    = "tr"
+    description = "Son çıkan yabancı dizi ve filmleri yabancidizi' de izle. En yeni yabancı film ve diziler, türkçe altyazılı yada dublaj olarak 1080p kalitesinde hd izle."
+    status  = 1
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=yabancidizi.tv&sz=%size%"
+}
